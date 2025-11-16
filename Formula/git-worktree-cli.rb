1a3f3c2a63f41cb4ef2039909e518f6faccd1286
TinsFox <fox@tinsfox.com>
2025-11-16 10:18:23 +0800

    gwt 0.1.1

--- Formula/git-worktree-cli.rb
+++ Formula/git-worktree-cli.rb
@@ -1,8 +1,9 @@
 class GitWorktreeCli < Formula
   desc "Git Worktree CLI - A powerful command-line tool for managing Git worktrees"
   homepage "https://github.com/TinsFox/gwt"
-  url "https://github.com/TinsFox/gwt/archive/v1.0.0.tar.gz"
-  sha256 "b48e46f0a021f8f2ec7e4a27cd8592d8b4dc43cb5c5cfe7a5eeda6abba5fcc91"
+  url "https://github.com/TinsFox/gwt/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "a98532ecfa6d80bc00996475e990489be479fe06c0612228a91e902cd61215b0"
+  version "0.1.1"
   license "MIT"
   head "https://github.com/TinsFox/gwt.git", branch: "main"
 
